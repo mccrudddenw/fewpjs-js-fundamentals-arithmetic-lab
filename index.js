eb0398350f683db78ff9e0c42ae4fd2b0ae1c0cb
mccruddenw <wyatt.mccrudden@gmail.com>
2021-04-06 07:49:38 -0700

    commit tree e5835de2c0d1c1b8a6fb008ec41b67cfe818cbf9

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random()) +1;
+
+
+const num3 = 24;
+const num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max(7, 8, 4, 6, 13, 20, 12)
+// Write your code hereS
